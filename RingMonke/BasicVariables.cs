b3cb1916eae730a1d85030434e0e3be3f141f125
Juan Searn Leoson <tagmonkebusiness@gmail.com>
2025-06-29 11:58:08 -0400

    Add project files.

---
+++ RingMonke/BasicVariables.cs
@@ -0,0 +1,24 @@
+﻿namespace RingMonke
+{
+    public class BasicVariables
+    {
+        private const string Version = "1.0.0";
+        public const string VersionLocal = Version;
+        public string LocalBlockedIDs = "";
+        public string AccountID = "-";
+        private protected string SessionLoginToken = "-";
+        private static protected string AllowedTickets = null;
+
+        public static string GetSessionLoginTokenProtected(string AllowedTicket)
+        {
+            if (AllowedTickets.Contains(AllowedTicket))
+            { return "null"; }
+            else return "Invalid Security Headers";
+        }
+
+        public static void DisconnectFromNetwork()
+        {
+
+        }
+    }
+}
---
+++ RingMonke/Plugin.cs
@@ -0,0 +1,27 @@
+﻿namespace RingMonke
+{
+    [BepInEx.BepInPlugin("com.RingMonkeTeam.RingMonke.live", "RingMonke", BasicVariables.VersionLocal)]
+    internal class Plugin : BepInEx.BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            GorillaTagger.OnPlayerSpawned(OnGameInit);
+        }
+
+        private void OnGameInit()
+        {
+            System.Console.Title = $"RingMonke {BasicVariables.VersionLocal}";
+            UnityEngine.Debug.Log($"Ring Monke | Version {BasicVariables.VersionLocal}");      
+        }                         
+        private void SummonError(string issue)
+        {
+            bool Flag = issue == null;
+            if (!Flag)
+            {
+                GorillaNetworking.GorillaComputer.instance.GeneralFailureMessage(issue);
+                Photon.Pun.PhotonNetwork.Disconnect();
+                BasicVariables.DisconnectFromNetwork();
+            }
+        }
+    }
+}
